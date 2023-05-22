c01546601c8db7d2b2be688680196cfb4a84414b
Hoby Ratefy <hoby.ratefy@protonmail.com>
2023-05-22 20:07:34 +0900

    Add drawing feature

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ for (let i = 0; i < squareSize; i++) {
   row.classList.add("gridrow");
   for (let j = 0; j < squareSize; j++) {
     const square = document.createElement("div");
+    square.addEventListener("mouseenter", function() {
+      this.classList.add("square-focus");
+    });
+    square.addEventListener("mouseleave", function() {
+      this.classList.remove("square-focus");
+      this.classList.add("square-trail");
+    })
     square.classList.add("square");
     row.appendChild(square);
   }
