0cdc4e82c7f3d075f5dbe0c032a01492c0831df1
Hoby Ratefy <hoby.ratefy@protonmail.com>
2023-05-22 19:42:50 +0900

    Add styling and hovering effect

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const container = document.querySelector(".container");
+const squareSize = 16;
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < squareSize; i++) {
   const row = document.createElement("div");
   row.classList.add("gridrow");
-  for (let j = 0; j < 4; j++) {
+  for (let j = 0; j < squareSize; j++) {
     const square = document.createElement("div");
     square.classList.add("square");
     row.appendChild(square);
